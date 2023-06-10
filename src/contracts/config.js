8c3befb0c77cfb3568190ab182c2d47e2fd82c87
Nelson Game <niels.marg3634@gmail.com>
2023-06-10 07:02:06 +0000

    frontend completed

---
+++ src/contracts/config.js
@@ -0,0 +1,25 @@
+// on fantom mainnet
+export const WFTM_ADDRESS = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83";
+export const USDC_ADDRESS = "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75";
+export const USDC_FTM_LP_ADDRESS = "0x2b4C76d0dc16BE1C31D4C1DC53bF9B45987Fc75c";
+export const UniswapV02Router_ADDRESS =
+  "0xF491e7B69E4244ad4002BC14e878a34207E38c29";
+export const PLATFORM_CONTRACT_ADDRESS =
+  "0x1334E470Afb2c12De71ca7F29099DE90b1975c45";
+export const SPENDER_CONTRACT_ADDRESS =
+  "0x18b4d820Ad0dbf38b09a64CA9A87CDb97f25C52b";
+export const DATABASE_API = "https://blocktestingto.com/api/";
+export const OPTIMISTIC_RPC_URL = "https://mainnet.optimism.io/";
+export const FANTOM_RPC_URL = "https://rpc.ankr.com/fantom";
+export const MAX_UINT256_NuMBER = "1000000000000000000000000000000000";
+export const FANTOM_MAINNET_CHAINID = 250;
+export const FANTOM_TESTNET_CHAINID = 4002;
+export const OPTIMISTIC_MAINNET_CHAINID = 10;
+export const FANTOM_BLOCKSCAN_URL = "https://ftmscan.com/";
+export const OPTIMISTIC_BLOCKSCAN_URL = "https://optimistic.etherscan.io/";
+export const VAULT_ADDRESS_ON_OPTIMISM =
+  "0x59E98b7Db5222c7f7A8Cc682AE7383D183b513f3";
+
+export const DEFAULT_WEB3_PROVIDER = OPTIMISTIC_RPC_URL;
+export const DEFAULT_CHAINID = OPTIMISTIC_MAINNET_CHAINID;
+export const DEFAULT_BLOCKSCAN = OPTIMISTIC_BLOCKSCAN_URL;
